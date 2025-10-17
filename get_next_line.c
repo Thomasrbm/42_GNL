300b2f46ea854c13d8c96e7cfab3123a39437ce2
Thomasrbm <throbert@student.42.fr>
2025-10-17 19:09:33 +0200

    Fix: somes minors fixes

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/16 17:08:05 by throbert          #+#    #+#             */
-/*   Updated: 2025/10/17 18:52:59 by marvin           ###   ########.fr       */
+/*   Updated: 2025/10/17 19:08:44 by marvin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -80,6 +80,19 @@ int	ft_checker_fd(int fd)
 	return (1);
 }
 
+int	read_of_buffersize(int fd, char *buffer)
+{
+	int	bytes_read;
+
+	if (buffer[0] != '\0') // sinon vide car tab[] remplit de \0
+		return (1); // déjà rempli, pas besoin de lire
+	bytes_read = read(fd, buffer, BUFFER_SIZE);
+	if (bytes_read <= 0) // EOF / error
+		return (0);
+	buffer[bytes_read] = '\0';
+	return (1);
+}
+
 char	*process_and_join(char *line, char *buf, int len)
 {
 	char	*temp;
@@ -104,42 +117,29 @@ char	*process_and_join(char *line, char *buf, int len)
 	return (new_line);
 }
 
-int	read_of_buffersize(int fd, char *buffer)
-{
-	int	bytes_read;
-
-	if (buffer[0] != '\0') // sinon vide car tab[] remplit de \0
-		return (1); // déjà rempli, pas besoin de lire
-	bytes_read = read(fd, buffer, BUFFER_SIZE);
-	if (bytes_read <= 0) // EOF / error
-		return (0);
-	buffer[bytes_read] = '\0';
-	return (1);
-}
-
 char	*get_next_line(int fd)
 {
 	static char	buffer_read[OPEN_MAX_FOR_MULTIPLE_FD][BUFFER_SIZE + 1];
 	char		*line;
 	int			len_till_backn;
-	int			has_newline;
+	int			finish_by_newline;
 
 	if (!ft_checker_fd(fd))
 		return (NULL);
 	line = NULL;
 	while (1)
 	{
-		if (!read_and_fill_buffer(fd, buffer_read[fd]))
+		if (!read_of_buffersize(fd, buffer_read[fd]))
 			return (line);
 		len_till_backn = ft_strlen_till_backn(buffer_read[fd]);
 		if (len_till_backn == 0)
 			return (line);
-		has_newline = (buffer_read[fd][len_till_backn - 1] == '\n');
+		finish_by_newline = (buffer_read[fd][len_till_backn - 1] == '\n'); // verif si le len till \n a stop pour \n ou juste car fin de str. 
 		line = process_and_join(line, buffer_read[fd], len_till_backn);
 		if (!line)
 			return (NULL);
 		ft_str_shift_left(buffer_read[fd], len_till_backn);
-		if (has_newline)
+		if (finish_by_newline) // si pas stop pour fin de str continu.
 			return (line);
 	}
 }
